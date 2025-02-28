91718e40565e5fdfa4b201093f4c1269b73c1f86
Marksmanship <AustinK.Copeland@gmail.com>
2025-02-28 11:23:58 -0500

    adding spot ondemandnodepool

--- main.tf
+++ main.tf
@@ -5,3 +5,9 @@ terraform {
     }
   }
 }
+
+resource "spot_ondemandnodepool" "d2d-nodes" {
+  cloudspace_name       = "door-to-door-cloudspace"
+  server_class          = "gp.vs1.medium-iad"
+  desired_server_count  = 2
+}
